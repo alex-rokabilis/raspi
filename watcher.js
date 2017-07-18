32f7b349d27745b44810cb076366c728e33b3cae
Alex Rock <alekos_l2@hotmail.com>
2017-07-19 01:03:00 +0300

    watcher

---
+++ watcher.js
@@ -0,0 +1,15 @@
+const child_process = require('child_process');
+
+function watch() {
+    child_process.exec("git pull", (err, stdout) => {
+        if (stdout.startsWith("Already up-to-date")) {
+            console.log('no change');
+            setTimeout(watch, 5000)
+
+        } else {
+            console.log('change')
+        }
+    })
+}
+
+watch();
