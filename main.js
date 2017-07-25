ee71c8570cec2daace4d4a93da562799ffd81a66
Alex Rock <alekos_l2@hotmail.com>
2017-07-25 16:41:55 +0300

    main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-setInterval(()=>{
-    console.log("whatsup!")
-},2000)
+const child_process = require("child_process");
+
+child_process.spawn("./binaries/temp_monitor")
+    .on('data', (data) => console.log(data.toSting()))
